290ad8e5ef23b4a9865d8a6b3118f51923473644
fahiz <fahizkppullara@gmail.com>
2021-12-14 01:21:44 +0530

    highest bid

--- test_fahiz_kp.py
+++ test_fahiz_kp.py
@@ -23,31 +23,46 @@ def winning_position_amount(value: int) -> int:
     '''
     winning_position = 9*value
     return winning_position   
+    
 
 def generate_winning_position(position_values: list, sum_of_position_values: int) -> list:
     '''
     This function generates a random winning position for the players.
     It returns the winning position and it's value.
     '''
-    positions_list, winning_positions_list, winning_amounts = [], [], []
+    positions_list_lessthan_90, positions_list, winning_positions_list, winning_amounts = [], [], [], []
     sum_of_the_values = sum_of_position_values
     percentage_90 = winning_percentage_90(sum_of_the_values)
-    
+    for i in range(len(position_values)):
+            x  = winning_position_amount(position_values[i])
+            if x < percentage_90:
+                positions_list_lessthan_90.append(position_values[i])
+
     while True:
-        value_for_the_position = random.choice(position_values)
+        positions_list_lessthan_90_balance = []
+        balance_winning_amount = percentage_90
+        value_for_the_position = random.choice(positions_list_lessthan_90)
         winning_amount = winning_position_amount(value_for_the_position)
+        balance_winning_amount = balance_winning_amount  - sum(winning_amounts)
+        if balance_winning_amount < winning_amount:
+            for j in positions_list_lessthan_90:
+                # this for loop is to add the elements to statisfy the highest bid.
+                condition_value = (balance_winning_amount//9)
+                if j<= condition_value and condition_value >min(positions_list_lessthan_90):
+                    positions_list_lessthan_90_balance.append(j)
+            if len(positions_list_lessthan_90_balance) != 0:
+                value_for_the_position = random.choice(positions_list_lessthan_90_balance)
+                winning_amount = winning_position_amount(value_for_the_position)
         if winning_amount < percentage_90:
             #the win amount should less than 90% of the total bet value of the given List.
-        
             positions_list.append(winning_amount)
             if sum(positions_list)<= percentage_90:
                 #sum of the total winning amount should not exceed 90% of the total bet value of the given.
                 
                 winning_positions_list.append(value_for_the_position)
-                winning_amounts.append(winning_amount)
+                winning_amounts.append(winning_amount)   
             else:
-                break
-            
+                break      
     return winning_positions_list, winning_amounts
 
 if __name__ == "__main__":
@@ -59,3 +74,5 @@ if __name__ == "__main__":
     print('Winning Amounts:', winning_amounts)
     commission = owner_profit(sum_of_position_values)
     print('Owner Profit:', commission)
+    print('Highest Bid:', sum(winning_amounts))
+
